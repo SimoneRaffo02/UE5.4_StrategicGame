0fe9d640206ae6369c24b1062bf137a28dd1bc63
SimoneRaffo02 <raffo.simone02@gmail.com>
2025-03-17 20:10:00 +0100

    Re-Creation of Project in Version 5.4

---
+++ UE_Game/Source/UE_Game/Public/GameField.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "Tile.h"
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "GameField.generated.h"
+
+UCLASS()
+class UE_GAME_API AGameField : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AGameField();
+
+	void GenerateField();
+
+	void SetFieldSize(int32 NewFieldSize);
+
+	int32 GetTileSize();
+
+	FVector GetRelativeLocationByXY(int32 X, int32 Y);
+
+	FVector2D GetPosition(const FHitResult& Hit);
+
+protected:
+	virtual void BeginPlay() override;
+
+	UPROPERTY(Transient)
+	TMap<FVector2D, ATile*> Field;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	int32 TileSize;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	int32 FieldSize;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Tile")
+	TSubclassOf<ATile> TileClass;
+};
---
+++ UE_Game/Source/UE_Game/UE_Game.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UE_Game.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UE_Game, "UE_Game" );
