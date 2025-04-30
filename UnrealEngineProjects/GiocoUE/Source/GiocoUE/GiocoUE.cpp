f248176ba9dfccbecfbd84e86aeb1448dda1d3d0
SimoneRaffo02 <raffo.simone02@gmail.com>
2025-04-30 23:06:55 +0200

    Delete UnrealEngineProjects directory

--- UnrealEngineProjects/GiocoUE/Source/GiocoUE/GiocoUE.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "GiocoUE.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GiocoUE, "GiocoUE" );
